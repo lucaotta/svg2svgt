0280acbd4596fabbd13b2758c5927df3ccaaf6af
Basil Peace <grv87@yandex.ru>
2019-03-02 05:19:32 +0300

    test: fix tests under Windows
    * Add shared DLL to `PATH`
    * Create output directory
    * Fix invalid `del` argument

--- test/run_tests.bat
+++ test/run_tests.bat
@@ -1,15 +1,20 @@
 set APP=%1
-del /y testdata\output\complex\*.*
+del /q testdata\output\complex\*.*
+md testdata\output\complex
 for %%f in (testdata\input\complex\*.svg) do %APP% %%f testdata\output\complex\%%~nxf
 
-del /y testdata\output\filters\*.*
+del /q testdata\output\filters\*.*
+md testdata\output\filters
 for %%f in (testdata\input\filters\*.svg) do %APP% %%f testdata\output\filters\%%~nxf
 
-del /y testdata\output\gradients\*.*
+del /q testdata\output\gradients\*.*
+md testdata\output\gradients
 for %%f in (testdata\input\gradients\*.svg) do %APP% %%f testdata\output\gradients\%%~nxf
 
-del /y testdata\output\shapes\*.*
+del /q testdata\output\shapes\*.*
+md testdata\output\shapes
 for %%f in (testdata\input\shapes\*.svg) do %APP% %%f testdata\output\shapes\%%~nxf
 
-del /y testdata\output\misc\*.*
+del /q testdata\output\misc\*.*
+md testdata\output\misc
 for %%f in (testdata\input\misc\*.svg) do %APP% %%f testdata\output\misc\%%~nxf
